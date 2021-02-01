912f94964776b718fe0444cdfc47b4427d788030
jJessica Wren <jessica.wren@ou.edu>
2021-02-01 15:46:58 -0600

    Finished Participation: Student

--- 3013-Review/Student/Program.cs
+++ 3013-Review/Student/Program.cs
@@ -1,15 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
-namespace Student
+/// <summary>
+/// @Author Instructor Ackerman's Project
+/// </summary>
+
+namespace StudentExample
 {
     class Program
     {
+        static List<Student> students = new List<Student>();
         static void Main(string[] args)
         {
-            string[] lines = File.ReadAllLines("studentdataRandom100Rows.csv");
+            string[] lines = File.ReadAllLines("studentDataRandom100Rows.csv");
+            //Student s = new Student(5, "Joe", "Joe", "Blue");
+            //Student s2 = new Student();
+            //s2.FirstName = "Joe";
+            //s2.LastName = "Joe";
 
-            //C:\Users\jessi\Downloads\studentdataRandom100Rows.csv
+            //foreach (var line in lines)
+            //{
+
+            //    Console.WriteLine(line);
+            //}
+
+            //start at 1 (int i = 1) bc 0 would be the first line (the headings)
+            for (int i = 1; i < lines.Length; i++)
+            {
+                //   0          1   2    3
+                //8203029752,Conni,Odo,Purple
+                string line = lines[i];
+                var pieces = line.Split(',');
+                Int64 id = Convert.ToInt64(pieces[0]);
+
+                Student currentStudent = new Student(Convert.ToInt64(pieces[0]), pieces[1], pieces[2], pieces[3]);
+
+                students.Add(currentStudent); // takes our current student and adds them up to the list
+            }
+
+            PrintStudentsWithFavoriteColor("Blue");
+
+
+            PrintStudentsWithFavoriteColor("Puce");
+
+            //C:\Users\acke9387\Downloads\studentDataRandom100Rows.csv
+        }
+
+        private static void PrintStudentsWithFavoriteColor(string color)
+        {
+            Console.WriteLine(color);
+
+            foreach (var student in students)
+            {
+                if (student.FavoriteColor.ToLower() == color.ToLower())
+                {
+                    Console.WriteLine(student.CreateFullName());
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
--- 3013-Review/Student/Student.cs
+++ 3013-Review/Student/Student.cs
@@ -4,7 +4,56 @@ using System.Text;
 
 namespace StudentExample
 {
-    class Student
+    public class Student
     {
+        private string MadeUpThingForExample;
+        public Int64 ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FavoriteColor { get; set; }
+
+        /// <summary>
+        /// Default/Empty Constructor
+        /// </summary>
+        public Student()
+        {
+            ID = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            FavoriteColor = string.Empty;
+        }
+
+        /// <summary>
+        /// Overloaded constructor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="first"></param>
+        /// <param name="last"></param>
+        /// <param name="color"></param>
+        public Student(Int64 id, string first, string last, string color)
+        {
+            ID = id;
+            FirstName = first;
+            LastName = last;
+            FavoriteColor = color;
+        }
+
+        public Student(Int64 id) : this()
+        {
+            ID = id;
+            //FirstName = string.Empty;
+            //LastName = string.Empty;
+            //FavoriteColor = string.Empty;
+        }
+
+        public string CreateFullName()
+        {
+            // return LastName + ", " + FirstName
+            return $"{LastName}, {FirstName}";
+        }
+
     }
 }
