e095989186365a31c2d579aed1460924f054d9c1
jJessica Wren <jessica.wren@ou.edu>
2021-02-01 10:33:04 -0600

    Started StudentExample Participation

---
+++ 3013-Review/Student/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("studentdataRandom100Rows.csv");
+
+            //C:\Users\jessi\Downloads\studentdataRandom100Rows.csv
+        }
+    }
+}
